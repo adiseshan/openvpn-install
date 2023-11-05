e12e9056e0b802d552c096cb94e7bdf4fbce5221
Adiseshan <adiseshan.k@gmail.com>
2023-11-05 19:17:32 +0900

    ec2

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1071,20 +1071,20 @@ function newClient() {
 	echo ""
 	echo "Tell me a name for the client."
 	echo "The name must consist of alphanumeric character. It may also include an underscore or a dash."
-
-	until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
-		read -rp "Client name: " -e CLIENT
-	done
+	CLIENT="mac-adi"
+	# until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
+	# 	read -rp "Client name: " -e CLIENT
+	# done
 
 	echo ""
 	echo "Do you want to protect the configuration file with a password?"
 	echo "(e.g. encrypt the private key with a password)"
 	echo "   1) Add a passwordless client"
 	echo "   2) Use a password for the client"
-
-	until [[ $PASS =~ ^[1-2]$ ]]; do
-		read -rp "Select an option [1-2]: " -e -i 1 PASS
-	done
+	PASS=1
+	# until [[ $PASS =~ ^[1-2]$ ]]; do
+	# 	read -rp "Select an option [1-2]: " -e -i 1 PASS
+	# done
 
 	CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
 	if [[ $CLIENTEXISTS == '1' ]]; then
